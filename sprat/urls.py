8e125f724eb94ad4262cda63d69dba3a3d7425d8
Glenda Pinho <glendasp@gmail.com>
2024-08-14 14:03:53 -0700

    Include index url

--- sprat/urls.py
+++ sprat/urls.py
@@ -20,9 +20,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from sprat.tool.views import index_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index_view, name='index'),
     path('', include('tool.urls')),  # Include URLs from the tool app
 ]#+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
